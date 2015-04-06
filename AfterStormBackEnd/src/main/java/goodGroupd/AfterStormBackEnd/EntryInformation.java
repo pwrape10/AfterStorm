9f9682f9bdb95043c67a8084b1dcb1f43cfa1ecb
pwrape10 <pwrape10@ou.edu>
2015-04-06 18:13:33 -0500

    constructor and setter

--- AfterStormBackEnd/src/main/java/goodGroupd/AfterStormBackEnd/EntryInformation.java
+++ AfterStormBackEnd/src/main/java/goodGroupd/AfterStormBackEnd/EntryInformation.java
@@ -1,6 +1,7 @@
 package goodGroupd.AfterStormBackEnd;
 
 import org.mongodb.morphia.annotations.Entity;
+import java.util.ArrayList;
 
 @Entity(noClassnameStored=true)
 public class EntryInformation {
@@ -8,18 +9,39 @@ public class EntryInformation {
 	private String primaryName;
 	
 	private String address;
-	
-	private String city;
-	
+		
 	private int zipCode;
 	
-	private int phoneNumber;
-	
-	private String email;
-	
-	private String groupSize;
+	private String phoneNumber;
+		
+	private ArrayList<String> otherPeople;
 	
 	private boolean critical;
 	
 	private String comments;
+	
+
+	public EntryInformation(String name, String address, int zipCode, String phoneNumber, String comments ){
+		
+		this.primaryName = name;
+		this.address = address;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
+		this.comments = comments;
+		
+	}
+	
+	public EntryInformation(String name, String address, int zipCode, String phoneNumber, String comments, ArrayList<String> additionalPeople ){
+		
+		this.primaryName = name;
+		this.address = address;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
+		this.comments = comments;
+		this.otherPeople = additionalPeople;
+	}
+	
+	public void isCritical(boolean criticalStatus){
+		this.critical = criticalStatus;
+	}
 }
